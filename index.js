e05df336b981658771d5fed334576945c6268254
Rohan Shinde <rohanshinde81136@gmail.com>
2021-06-14 13:45:35 +0530

    changed tablet view

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const menu = document.querySelector('.hamburger')
-const menuItems = document.querySelector('.list-items')
+const menuItems = document.querySelector('.nav-items')
 
 menu.addEventListener('click', () => {
     menuItems.classList.toggle('active')
+
+    menuItems.classList.contains('active') ? menu.src = '/images/icon-close.svg' : menu.src = '/images/icon-hamburger.svg'
+
 })
 
 window.addEventListener('click', (e) => {
     if (e.target.closest('main') || e.target.closest('footer') || e.target.closest('.link')) {
-        menuItems.classList.remove('active')
+        setTimeout(() => {
+            menuItems.classList.remove('active')
+            menu.src = '/images/icon-hamburger.svg'
+        })
+
     }
 
 })
